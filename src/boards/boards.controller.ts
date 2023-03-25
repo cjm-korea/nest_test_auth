8beaa223f00223a9cb56e2e26acdd5e778983580
CJM <whdals9514@naver.com>
2023-03-26 00:51:05 +0900

    auth

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+    constructor(private boardService: BoardsService) { }
+
+    @Get('/')
+    getAllBoard(): Board[] {
+        return this.boardService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto
+    ): Board {
+        return this.boardService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+        return this.boardService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(@Param('id') id:string, @Body('status') status: BoardStatus): Board {
+        return this.boardService.updateBoardStatus(id, status)
+    }
+}
